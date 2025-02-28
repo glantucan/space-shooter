cb7a35c1b64c215122fd699e7c2df35be06c5894
Oscar Espeso Gil <glantucan@gmail.com>
2025-02-28 10:25:07 +0000

    Added asset paths js file

---
+++ src/components/gamemodules/gameAssets.js
@@ -0,0 +1,15 @@
+
+
+export default const assets = {
+  player: 'assets/player-ship/player.png',
+  enemies: {
+    tier1: 'assets/enemy-ships/enemy01.png',
+    tier2: 'assets/enemy-ships/enemy02.png',
+    tier2: 'assets/enemy-ships/enemy03.png',
+  },
+  background: {
+    far: '/assets/images/backgrounds/stars-far.png',
+    mid: '/assets/images/backgrounds/stars-mid.png',
+    near: '/assets/images/backgrounds/stars-mid.png',
+  },
+};
